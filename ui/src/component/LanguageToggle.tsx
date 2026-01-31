548beb44293d0f709b44f05d4663c7da99de873d
OlegKolchin <commshepard@gmail.com>
2026-01-31 21:32:17 +0500

    Fixed language icon

--- ui/src/component/LanguageToggle.tsx
+++ ui/src/component/LanguageToggle.tsx
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import { Button } from '@mui/material';
+import LanguageIcon from '@mui/icons-material/Language';
 import { useDefaultSettings } from '../context/DefaultSettingsContext';
 
 const LanguageToggle: React.FC = () => {
@@ -10,6 +10,7 @@ const LanguageToggle: React.FC = () => {
         <Button
             onClick={toggleLanguage}
             variant="contained"
+            startIcon={<LanguageIcon />}
             sx={{
                 position: 'fixed',
                 top: 16,
@@ -18,12 +19,14 @@ const LanguageToggle: React.FC = () => {
                 fontWeight: 'bold',
                 zIndex: 1000,
                 backgroundColor: '#4169E1',
+                fontFamily: "'Poppins', 'Inter', sans-serif",
+                letterSpacing: '0.5px',
                 '&:hover': {
                     backgroundColor: '#1E90FF',
                 },
             }}
         >
-            {language === 'en' ? '🇷🇺 RU' : '🇬🇧 EN'}
+            {language === 'en' ? 'RU' : 'EN'}
         </Button>
     );
 };
